ce70f0b460e1acc99007312a78d461ac71306928
Pedro Henrique <pedrohenrique@MacBook-Air-de-Pedro.local>
2022-09-01 15:36:18 -0300

    Project created, create UserController class, and add HTTP methods

---
+++ src/main/java/com/dev/mobileappws/ui/controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.dev.mobileappws.ui.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+    @GetMapping
+    public String getUser() {
+        return "getUser() was called";
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "createUser() was called";
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "updateUser() was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "deleteUser() was called";
+    }
+
+}
